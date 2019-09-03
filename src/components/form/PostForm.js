abbe8223d8ea0592081114b267e38917788a72c5
shohagkhan <4shohag@gmail.com>
2019-09-03 16:36:17 +0600

    Form Part 2 Complete

--- src/components/form/PostForm.js
+++ src/components/form/PostForm.js
@@ -3,7 +3,10 @@ const initialState = {
     name: '',
     email: '',
     password: '',
-    bio: ''
+    bio: '',
+    country: '',
+    gender: '',
+    skills: []
 }
 export default class PostForm extends Component {
     constructor(props) {
@@ -14,10 +17,30 @@ export default class PostForm extends Component {
         ...initialState
     }
     changeHandler = (event) => {
-        this.setState({
-            // Q. Why Third Bracket
-            [event.target.name]: event.target.value
-        })
+        if (event.target.type === 'checkbox') {
+
+            if (event.target.checked) {
+                this.setState({
+                    // Q. Why Full State Called Here?
+                    ...this.state,
+                    skills: this.state.skills.concat(event.target.value)
+                })
+            } else {
+                this.setState({
+                    ...this.state,
+                    skills: this.state.skills.filter(skill => skill !== event.target.value)
+                })
+            }
+
+
+        } else {
+            this.setState({
+                // Q. Why Third Bracket?
+                [event.target.name]: event.target.value
+            })
+            console.log(event.target.value)
+
+        }
     }
     submitHandler = (event) => {
         event.preventDefault()
@@ -25,7 +48,6 @@ export default class PostForm extends Component {
         this.myForm.current.reset()
         this.setState({
             ...initialState,
-            // email: "test@gmail.com" I just create it only for test perpose
         })
     }
     render() {
@@ -47,10 +69,49 @@ export default class PostForm extends Component {
                     <label htmlFor="bio">Enter Your bio</label>
                     <textarea className="form-control" type="text" placeholder="Enter bio" name="bio" id="bio" value={this.state.bio} onChange={this.changeHandler} />
                 </div>
+                <div className="form-group">
+                    <label htmlFor="country">Country</label>
+                    <select className="form-control" name="country" id="country" onChange={this.changeHandler}>
+                        <option value="Bangladesh">Bangladesh</option>
+                        <option value="India">India</option>
+                        <option value="Pakistan">Pakistan</option>
+                        <option value="Brazil">Brazil</option>
+                        <option value="Argentina">Argentina</option>
+                        <option value="Australia">Australia</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <div className="form-check form-check-inline">
+                        <label htmlFor="gender1">Male</label>
+                        <input type="radio" className="form-check-label" name="gender" id="gender1" value="Male" onChange={this.changeHandler} />
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <label htmlFor="gender2">Female</label>
+                        <input type="radio" className="form-check-label" name="gender" id="gender2" value="Female" onChange={this.changeHandler} />
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <label htmlFor="gender3">Others</label>
+                        <input type="radio" className="form-check-label" name="gender" id="gender3" value="Others" onChange={this.changeHandler} />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="HTML" name="skills" value="HTML" onChange={this.changeHandler} />
+                        <label className="custom-control-label" htmlFor="HTML">HTML</label>
+                    </div>
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="CSS" name="skills" value="CSS" onChange={this.changeHandler} />
+                        <label className="custom-control-label" htmlFor="CSS">CSS</label>
+                    </div>
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="JavaScript" name="skills" value="JavaScript" onChange={this.changeHandler} />
+                        <label className="custom-control-label" htmlFor="JavaScript">JavaScript</label>
+                    </div>
+                </div>
                 <div>
                     <button className="btn btn-primary" type="submit">Submit</button>
                 </div>
-            </form>
+            </form >
         )
     }
 }
