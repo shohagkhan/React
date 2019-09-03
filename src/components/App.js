21bf9edeb1308863ef72cccbbbfe0d6ec492f4ed
shohagkhan <4shohag@gmail.com>
2019-09-03 20:16:19 +0600

    Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,11 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import PostForm from './form/PostForm';
+
 export default class App extends Component {
-    state = {
-        posts: []
-    }
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                this.setState({
-                    posts: response.data
-                })
-            })
-            .catch(error => console.log(error))
-    }
     render() {
-        // let { posts } = this.state
-        if (this.state.posts.length === 0) {
-            return <h1>Loading...</h1>
-        } else {
-            return (
-                <div className='container' >
-                    <div className="row">
-                        <div className="col-sm-8 offset-sm-2">
-                            <PostForm />
-                        </div>
-                    </div>
-                </div>
-            )
-        }
+        return (
+            <div>
+               <h1>ok</h1> 
+            </div>
+        )
     }
 }
